a4c9921804924bed6faf569d0142062731c31b4d
Rishavsingh-jbn <rishavsinghjbn@gmail.com>
2026-02-02 20:00:58 +0530

    feat(client): initialize React app with Vite and Tailwind CSS
    - Added package.json for client with dependencies and scripts for development, build, linting, and preview.
    - Created Vite configuration file for React and Tailwind CSS integration.
    - Added main application component (App.jsx) with basic structure and Tailwind CSS styling.
    - Included SVG assets for Vite and React logos.
    - Set up index.css to import Tailwind CSS styles.
    - Implemented main entry point (main.jsx) to render the App component.
    - Added public assets including vite.svg.
    
    feat(server): set up Express server with MongoDB connection
    
    - Updated package.json to include bcrypt and jsonwebtoken dependencies.
    - Created server entry point (index.js) to initialize the server and connect to MongoDB.
    - Implemented database connection logic in a separate module (db/index.js).
    - Added app.js as the main application file for Express setup.
    - Configured nodemon for development environment.

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-300 flex text-4xl text-red-600'>
+        
+        
+        Click on the Vite and React logos to learn more
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import "dotenv/config"; // this automatically runs dotenv.config()
+import { app } from "./app.js";
+// import express from "express";
+import connectDB from "./db/index.js";
+
+// const app = express();
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(
+        `Server running at http://localhost:${port}  \n Jai Bhole Nath`,
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB CONNECTION ERROR:", error);
+  });
